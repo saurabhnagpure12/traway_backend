ed3fdb6d3ac6f778b72a8be3103d9cac11b35010
PavanKumar <51289314+Gumparthypavankumar@users.noreply.github.com>
2021-06-26 20:29:20 +0530

    Update UserLocationSearch model also update functionality

--- app/controllers/MapsController.js
+++ app/controllers/MapsController.js
@@ -1,13 +1,17 @@
 // Load Models
 const UserSearchedLoactions = require("../models/UserLocationSearch");
 const UserRoute = require("../models/UserRoute");
-const User = require('../models/User');
+const User = require("../models/User");
+const { validationResult } = require("express-validator");
 
 //Save recent user location searches
 exports.storeUserLocationSearch = async function (req, res) {
   try {
-    // TODO: Add Validations for post data
-    const { location_id } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { location_id, location_name, location_address } = req.body;
     const checkUser = await User.findById(req.user.id);
     if (!checkUser) {
       return res.status(400).json({ data: [{ msg: "User Does Not Exist" }] });
@@ -17,15 +21,26 @@ exports.storeUserLocationSearch = async function (req, res) {
     if (!user) {
       const createdUserLocations = await new UserSearchedLoactions({
         user_id: req.user.id,
-        recent_search_locations: [location_id],
+        recent_search_locations: [
+          {
+            location_id,
+            location_name,
+            location_address,
+          },
+        ],
       }).save();
-      return res.status(200).json({ data: {'msg' : 'Location Created Successfully'}});
-    }
-    else {
+      return res
+        .status(200)
+        .json({ data: { msg: "Location Created Successfully" } });
+    } else {
       // Grab all previous locations
       const locations = user.recent_search_locations;
       // enQueue the current location
-      locations.unshift(location_id);
+      locations.unshift({
+        location_id,
+        location_name,
+        location_address,
+      });
       // check if you have more than 5 locations
       if (locations.length > 5) {
         // Delete all elements from 5 to arrayLength
@@ -41,9 +56,10 @@ exports.storeUserLocationSearch = async function (req, res) {
         }
       );
     }
-    return res.status(200).json({ data: [{'msg' : 'Location Updated Successfully'}]});
+    return res
+      .status(200)
+      .json({ data: [{ msg: "Location Updated Successfully" }] });
   } catch (err) {
-
     res.status(500).json({
       data: [{ msg: "Error occurred" }],
     });
@@ -71,35 +87,45 @@ exports.storeRoute = async function (req, res) {
             }
           }
     */
-   const checkUser = await User.findById(req.user.id);
+    const checkUser = await User.findById(req.user.id);
     if (!checkUser) {
       return res.status(400).json({ data: [{ msg: "User Does Not Exist" }] });
     }
     const { start, end } = req.body;
     // Since the document is returned holding into variable there is no other purpose or no usage of it in further code
-    const savedLocation = await new UserRoute({user_id: req.user.id, start, end }).save();
-    return res.status(200).json({ data: [{ 'msg' : 'User Route Added Successfully' }]})
+    const savedLocation = await new UserRoute({
+      user_id: req.user.id,
+      start,
+      end,
+    }).save();
+    return res
+      .status(200)
+      .json({ data: [{ msg: "User Route Added Successfully" }] });
   } catch (err) {
     return res.status(500).json({
-      data: [{ msg : "Error occurred"}],
+      data: [{ msg: "Error occurred" }],
     });
   }
 };
 
-exports.fetchRoutes = async function(req, res){
-  try{
+exports.fetchRoutes = async function (req, res) {
+  try {
     // Fetch current users recent locations
-    const UserRecentLocations = await UserSearchedLoactions.findOne({ user_id: req.user.id });
-    // Check if there are any locations 
-    if(UserRecentLocations)
-      return res.status(200).json({ data: UserRecentLocations.recent_search_locations });
-    return res.status(200).json({ data: [{ msg : 'User has No Searched Locations' }] });
-  }
-  catch(err){
+    const UserRecentLocations = await UserSearchedLoactions.findOne({
+      user_id: req.user.id,
+    });
+    // Check if there are any locations
+    if (UserRecentLocations)
+      return res
+        .status(200)
+        .json({ data: UserRecentLocations.recent_search_locations });
+    return res
+      .status(200)
+      .json({ data: [{ msg: "User has No Searched Locations" }] });
+  } catch (err) {
     console.log(err.message);
     return res.status(500).json({
-      data: [{ msg : "Error occurred"}],
+      data: [{ msg: "Error occurred" }],
     });
   }
-}
-
+};
