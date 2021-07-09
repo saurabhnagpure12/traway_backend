753f924674995e849e52a3d847a32d639621f653
PavanKumar <51289314+Gumparthypavankumar@users.noreply.github.com>
2021-07-09 17:55:56 +0530

    Added Reaction model and routes and added functionality

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,16 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 var authRouter = require("./api/auth.js");
 var userRouter = require("./api/user.js");
 var mapsRouter = require("./api/maps.js");
 var circleRouter = require("./api/circle.js");
-
+var reactionRouter = require("./api/reaction.js");
 
 router.use("/authenticate", authRouter);
 router.use("/users", userRouter);
 router.use("/maps", mapsRouter);
 router.use("/circles", circleRouter);
-
-
+router.use("/reactions", reactionRouter);
 
 module.exports = router;
