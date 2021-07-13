7249dc88ec4eb0b5954ab7de4b71bc50e6b324d0
Nitish <nitish0399@hotmail.com>
2021-07-13 20:05:16 +0530

    downgrade socket.io library

--- app.js
+++ app.js
@@ -114,6 +114,6 @@ io.on('connection', function (client) {
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server started at PORT: ${PORT}`);
 });
